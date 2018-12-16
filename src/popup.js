f68090cbd22ea0c90fe1d5416aa747f0d52a8085
Rizwan Raza <rizwan.raza987@gmail.com>
2018-12-16 17:43:29 +0530

    Tweaked JS

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('form').addEventListener('submit', contact);
+  document.getElementsByTagName("html")[0].style.width="384px";
+  document.getElementsByTagName("body")[0].style.width="384px";
+});
+
 function contact(e) {
   e.preventDefault();
 
@@ -6,7 +12,7 @@ function contact(e) {
   });
 
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       document
         .querySelectorAll("#progress, .prevent-overlay")
@@ -15,6 +21,8 @@ function contact(e) {
         });
       var object = JSON.parse(this.responseText);
       alert(object.message);
+      e.target.reset();
+      window.close();
       //   console.log(this.responseText);
     }
   };
@@ -23,14 +31,14 @@ function contact(e) {
   let f = e.target;
   xhttp.send(
     "name=" +
-      f.name.value.trim() +
-      "&email=" +
-      f.email.value.trim() +
-      "&number=" +
-      f.number.value.trim() +
-      "&company=" +
-      f.company.value.trim() +
-      "&message=" +
-      f.message.value.trim()
+    f.name.value.trim() +
+    "&email=" +
+    f.email.value.trim() +
+    "&number=" +
+    f.number.value.trim() +
+    "&company=" +
+    f.company.value.trim() +
+    "&message=" +
+    f.message.value.trim()
   );
 }
